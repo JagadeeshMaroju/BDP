363c60722f3475ed0019e3f004e15a27de35db6a
Jagadeesh Maroju <jagadeesh.maroju19@gmail.com>
2020-04-07 17:29:16 -0500

    code

---
+++ ICP_10/Source/SparkDataFrames.py
@@ -0,0 +1,48 @@
+import csv
+from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+
+spark = SparkSession \
+    .builder \
+    .appName("Python Spark SQL basic example") \
+    .getOrCreate()
+
+#question 1
+df = spark.read.csv(r"C:\Users\jagad\Downloads\survey.csv",header=True)
+df.createOrReplaceTempView("Survey")
+#question 2
+# df.write.option("header", "true").csv("spark_survey3.csv")
+#question 3
+# print(df.dropDuplicates().count())
+
+# df.groupBy(df.columns)\
+#     .count()\
+#     .where(f.col('count') > 1)\
+#     .select(f.sum('count'))\
+#     .show()
+#question 4
+
+spark.sql("select * from Survey where Gender = 'Male' or Gender = 'M' or Gender='male'").createTempView("Table_Male")
+spark.sql("select * from Survey where Gender = 'Female' or Gender = 'female'").createTempView("Table_Female")
+# spark.sql("select * from Table_Male union select * from Table_Female order by Country").show()
+
+#question 5
+# spark.sql("select treatment,count(*) as count from Survey group by treatment").show()
+
+
+#part 2 - question 1
+# spark.sql("select m.age,m.Country,m.Gender, m.treatment,f.Gender,f.treatment from Table_Male m join Table_Female f on m.Country = f.Country").show()
+# spark.sql("select sum(Age),count(Gender) from Survey").show()
+# spark.sql("select m.age,m.Country,m.Gender, m.treatment,f.Gender,f.treatment from Table_Male m left join Table_Female f on m.State = f.State").show()
+#part 2 - question 2
+spark.sql("select * from Survey ORDER BY Timestamp limit 13").createTempView("Test")
+spark.sql("select * from Test order by Timestamp desc limit 1").show()
+
+# def parseLine(row):
+#     csvdata = csv.reader([row], delimiter=',', quotechar='"')
+#     for i in csvdata:
+#         print(i)
+#
+#
+# dataRead_data = df.rdd.map(parseLine)
+# print(dataRead_data.collect())
